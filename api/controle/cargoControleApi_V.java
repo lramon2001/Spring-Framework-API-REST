d71046d4373664ea436bf10a5d01a2e99be4d8cc
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2022-10-17 16:09:05 -0300

    Create cargoControleApi_V.java

---
+++ api/controle/cargoControleApi_V.java
@@ -0,0 +1,60 @@
+package br.com.treinaweb.twprojetos.api.controles;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.treinaweb.twprojetos.api.dto.CargoDto;
+import br.com.treinaweb.twprojetos.entidades.Cargo;
+import br.com.treinaweb.twprojetos.servicos.CargoServico;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/v1/cargos")
+
+public class CargoControleApi {
+   @Autowired
+   private CargoServico cargoServico;
+
+   @GetMapping
+  
+   public List<br.com.treinaweb.twprojetos.entidades.Cargo> buscarTodos()
+   {
+    return cargoServico.buscarTodos();
+   }
+
+  @GetMapping("/{id}")
+
+  public Cargo buscaPorId(@PathVariable long id)
+  {
+   return this.cargoServico.buscarPorId(id);
+  }
+
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Cargo cadastrar(@RequestBody CargoDto cargo)
+  {
+    return this.cargoServico.cadastrar(cargo);
+  }
+
+  @PutMapping("/{id}")
+  public Cargo atualizar(@RequestBody CargoDto cargoDto, @PathVariable Long id)
+  {
+    return this.cargoServico.atualizar(cargoDto, id);
+  }
+     
+   
+  
+}
