c9bea37e02899801c9495822dac15aba179596ce
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2022-11-26 14:53:48 -0300

    Create SwaggerConfig.java

---
+++ config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package br.com.lucasramon.lrprojetos.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final String CONTATO_EMAIL = "lucasoliveirainor3105@gmail.com";
+    private static final String CONTATO_URL = "https://github.com/lramon2001";
+    private static final String CONTATO_NOME = "Lucas Ramon";
+    private static final String API_VERSAO = "1.0.0";
+    private static final String API_DESCRICAO = "API de Gerenciamento de Projetos";
+    private static final String API_TITULO = "LR projetos";
+    private static final String PACOTE_BASE = "br.com.lucasramon.lrprojetos.api";
+
+    public Docket getDocket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage(PACOTE_BASE))
+        .paths(PathSelectors.any())
+        .build().apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo(){
+        return new ApiInfoBuilder()
+        .title(API_TITULO)
+        .description(API_DESCRICAO)
+        .version(API_VERSAO)
+        .contact(new Contact(CONTATO_NOME,CONTATO_URL,CONTATO_EMAIL))
+        .build();
+    }
+
+    
+}
